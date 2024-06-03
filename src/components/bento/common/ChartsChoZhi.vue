77f4949aff525e766d580fbd3e0ab28e57ae1372
liqingyou <742597426@qq.com>
2024-06-03 13:46:13 +0800

    添加充值表格, 创建类型

--- src/components/bento/common/ChartsChoZhi.vue
+++ src/components/bento/common/ChartsChoZhi.vue
@@ -6,15 +6,20 @@ import type { ECharts, EChartsOption } from 'echarts'
 import { init } from 'echarts'
 import axios from 'axios'
 
+interface TjOnline {
+  date: string
+  onlineNum: number
+  google: number
+  apple: number
+  jl: number
+}
+
 const chartData = ref([])
 
 async function fetchContent() {
   try {
     const { data } = await axios.get('https://m3test.2loveyou.com/spin/test/player_read/online_data?limit=30&key=tj_online_1')
-    chartData.value = data.result.map((item: any) => ({
-      date: item.date,
-      onlineNum: item.onlineNum,
-    }))
+    chartData.value = data.result.map((item: TjOnline) => item)
     updateChart() // Call updateChart after fetching new data
   }
   catch (error) {
@@ -27,22 +32,25 @@ function updateChart() {
   const option: EChartsOption = {
     xAxis: {
       type: 'category',
-      data: chartData.value.map(item => item.date),
+      data: chartData.value.map((item: TjOnline) => item.date),
     },
     yAxis: {
       type: 'value',
     },
     series: [
       {
-        data: chartData.value.map(item => item.google),
+        data: chartData.value.map((item: TjOnline) => item.google),
+        name: 'google',
         type: 'line',
       },
       {
-        data: chartData.value.map(item => item.apple),
+        data: chartData.value.map((item: TjOnline) => item.apple),
+        name: 'apple',
         type: 'line',
       },
       {
-        data: chartData.value.map(item => item.jl),
+        data: chartData.value.map((item: TjOnline) => item.jl),
+        name: 'jl',
         type: 'line',
       },
     ],
